42966bd933700027a0e0633538bfdfb473ccd0d3
陈志伟 <chen.zhiwei@jyall.com>
2017-07-19 14:47:45 +0800

    增加个人页面

--- app/src/main/java/com/niuyun/hire/ui/index/fragment/MyFragment.java
+++ app/src/main/java/com/niuyun/hire/ui/index/fragment/MyFragment.java
@@ -2,8 +2,6 @@ package com.niuyun.hire.ui.index.fragment;
 
 import android.content.Intent;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.RatingBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -12,16 +10,8 @@ import com.niuyun.hire.base.BaseContext;
 import com.niuyun.hire.base.BaseFragment;
 import com.niuyun.hire.base.Constants;
 import com.niuyun.hire.base.EventBusCenter;
-import com.niuyun.hire.ui.activity.AboutActivity;
-import com.niuyun.hire.ui.activity.AccountSafetyActivity;
-import com.niuyun.hire.ui.activity.CommitRealNameActivity;
 import com.niuyun.hire.ui.activity.LoginActivity;
-import com.niuyun.hire.ui.activity.MyMemberRankActivity;
-import com.niuyun.hire.ui.activity.OrderListActivity;
-import com.niuyun.hire.ui.activity.PersonInformationActivity;
-import com.niuyun.hire.ui.activity.ShoppingAddressActivity;
 import com.niuyun.hire.utils.DialogUtils;
-import com.niuyun.hire.utils.ImageLoadedrManager;
 import com.niuyun.hire.view.CircularImageView;
 
 import org.greenrobot.eventbus.EventBus;
@@ -29,46 +19,23 @@ import org.greenrobot.eventbus.EventBus;
 import butterknife.BindView;
 
 /**
+ * 个人页面
  * Created by chen.zhiwei on 2017-6-19.
  */
 
 public class MyFragment extends BaseFragment implements View.OnClickListener {
     @BindView(R.id.iv_head)
     CircularImageView ivHead;
-    @BindView(R.id.tv_member)
-    TextView tvMember;
-    @BindView(R.id.iv_image_order)
-    ImageView ivImageOrder;
-    @BindView(R.id.rl_order)
-    RelativeLayout rlOrder;
-    @BindView(R.id.iv_safe)
-    ImageView ivSafe;
-    @BindView(R.id.rl_safety)
-    RelativeLayout rlSafety;
-    @BindView(R.id.iv_location)
-    ImageView ivLocation;
-    @BindView(R.id.rl_location)
-    RelativeLayout rlLocation;
-    @BindView(R.id.iv_mumber)
-    ImageView ivMumber;
-    @BindView(R.id.rl_member)
-    RelativeLayout rlMember;
-    @BindView(R.id.iv_real_name)
-    ImageView ivRealName;
-    @BindView(R.id.rl_real_name)
-    RelativeLayout rlRealName;
-    @BindView(R.id.iv_abount)
-    ImageView ivAbount;
-    @BindView(R.id.rl_about)
-    RelativeLayout rlAbout;
-    @BindView(R.id.tv_setting)
-    TextView tv_setting;
-    @BindView(R.id.rl_quit_login)
-    RelativeLayout rl_quit_login;
     @BindView(R.id.tv_user_name)
     TextView tv_user_name;
-    @BindView(R.id.rb_rank)
-    RatingBar rb_rank;
+    @BindView(R.id.rl_video_resume)
+    RelativeLayout rl_video_resume;
+    @BindView(R.id.rl_stars)
+    RelativeLayout rl_stars;
+    @BindView(R.id.rl_watched)
+    RelativeLayout rl_watched;
+    @BindView(R.id.rl_setting)
+    RelativeLayout rl_setting;
 
     @Override
     protected int getContentViewLayoutId() {
@@ -77,14 +44,10 @@ public class MyFragment extends BaseFragment implements View.OnClickListener {
 
     @Override
     protected void initViewsAndEvents() {
-        rlOrder.setOnClickListener(this);
-        rlSafety.setOnClickListener(this);
-        rlLocation.setOnClickListener(this);
-        rlMember.setOnClickListener(this);
-        rlRealName.setOnClickListener(this);
-        rlAbout.setOnClickListener(this);
-        tv_setting.setOnClickListener(this);
-        rl_quit_login.setOnClickListener(this);
+        rl_video_resume.setOnClickListener(this);
+        rl_stars.setOnClickListener(this);
+        rl_watched.setOnClickListener(this);
+        rl_setting.setOnClickListener(this);
     }
 
     @Override
@@ -93,23 +56,6 @@ public class MyFragment extends BaseFragment implements View.OnClickListener {
     }
 
     private void setUserInfo() {
-        if (BaseContext.getInstance().getUserInfo() != null) {
-            rl_quit_login.setVisibility(View.VISIBLE);
-            tv_user_name.setText(BaseContext.getInstance().getUserInfo().nickname);
-            if (BaseContext.getInstance().getUserInfo().vipgrade > 0) {
-                rb_rank.setNumStars(BaseContext.getInstance().getUserInfo().vipgrade);
-                rb_rank.setVisibility(View.VISIBLE);
-            } else {
-                rb_rank.setVisibility(View.GONE);
-            }
-            ImageLoadedrManager.getInstance().display(getActivity(), BaseContext.getInstance().getUserInfo().headimg, ivHead, R.mipmap.ic_head_default, R.mipmap.ic_head_default);
-        } else {
-            rl_quit_login.setVisibility(View.GONE);
-            tv_user_name.setText("");
-            rb_rank.setVisibility(View.GONE);
-//            ImageLoadedrManager.getInstance().display(getActivity(), "", ivHead, R.mipmap.ic_head_default, R.mipmap.ic_head_default);
-            ivHead.setImageResource(R.mipmap.ic_head_default);
-        }
     }
 
     @Override
@@ -141,66 +87,22 @@ public class MyFragment extends BaseFragment implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.rl_order:
-                //订单
-                if (BaseContext.getInstance().getUserInfo() == null) {
-                    startActivity(new Intent(getActivity(), LoginActivity.class));
-                    return;
-                }
-                startActivity(new Intent(getActivity(), OrderListActivity.class));
-                break;
-            case R.id.rl_safety:
-                //账户安全
-                if (BaseContext.getInstance().getUserInfo() == null) {
-                    startActivity(new Intent(getActivity(), LoginActivity.class));
-                    return;
-                }
-                startActivity(new Intent(getActivity(), AccountSafetyActivity.class));
-                break;
-            case R.id.rl_location:
-                //收货地址
-                if (BaseContext.getInstance().getUserInfo() == null) {
-                    startActivity(new Intent(getActivity(), LoginActivity.class));
-                    return;
-                }
-                startActivity(new Intent(getActivity(), ShoppingAddressActivity.class));
-
-                break;
-            case R.id.rl_member:
-                //会员
-                if (BaseContext.getInstance().getUserInfo() == null) {
-                    startActivity(new Intent(getActivity(), LoginActivity.class));
-                    return;
-                }
-                startActivity(new Intent(getActivity(), MyMemberRankActivity.class));
-                break;
-            case R.id.rl_real_name:
-                //实名制
-                if (BaseContext.getInstance().getUserInfo() == null) {
-                    startActivity(new Intent(getActivity(), LoginActivity.class));
-                    return;
-                }
-                startActivity(new Intent(getActivity(), CommitRealNameActivity.class));
-                break;
-            case R.id.rl_about:
-                //关于
-                startActivity(new Intent(getActivity(), AboutActivity.class));
-                break;
-            case R.id.tv_setting:
+            case R.id.rl_setting:
                 //设置
-                if (BaseContext.getInstance().getUserInfo() == null) {
-                    startActivity(new Intent(getActivity(), LoginActivity.class));
-                    return;
-                }
-                startActivity(new Intent(getActivity(), PersonInformationActivity.class));
-                break;
-            case R.id.rl_quit_login:
-                //退出登录
-                if (BaseContext.getInstance().getUserInfo() == null) {
+//                if (BaseContext.getInstance().getUserInfo() == null) {
 //                    startActivity(new Intent(getActivity(), LoginActivity.class));
-                    return;
-                }
-                showExitDialog();
+//                    return;
+//                }
+//                startActivity(new Intent(getActivity(), PersonInformationActivity.class));
+                break;
+            case R.id.rl_video_resume:
+                //视频简历
+                break;
+            case R.id.rl_stars:
+                //我的关注
+                break;
+            case R.id.rl_watched:
+                //谁看过我
                 break;
         }
     }

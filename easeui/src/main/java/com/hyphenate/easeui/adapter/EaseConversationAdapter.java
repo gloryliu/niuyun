b7aff41537c7b1cbe4991282c43f2beb4638b5ef
陈志伟 <chen.zhiwei@jyall.com>
2017-07-27 18:16:41 +0800

    删除so文件

--- easeui/src/main/java/com/hyphenate/easeui/adapter/EaseConversationAdapter.java
+++ easeui/src/main/java/com/hyphenate/easeui/adapter/EaseConversationAdapter.java
@@ -20,6 +20,7 @@ import com.hyphenate.chat.EMConversation.EMConversationType;
 import com.hyphenate.chat.EMGroup;
 import com.hyphenate.chat.EMMessage;
 import com.hyphenate.easeui.EaseUI;
+import com.hyphenate.easeui.R;
 import com.hyphenate.easeui.domain.EaseAvatarOptions;
 import com.hyphenate.easeui.domain.EaseUser;
 import com.hyphenate.easeui.model.EaseAtMessageHelper;
